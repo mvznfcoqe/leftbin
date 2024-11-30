4f5e6a2001aa61ceea1b9c006379d09fc6d79cfc
Dmitri Moskalenko <frkam@icloud.com>
2024-11-30 10:30:45 +0300

    fix(bin): fix up-resume

--- apps/bin/src/models/service/hh/up-resume.ts
+++ apps/bin/src/models/service/hh/up-resume.ts
@@ -11,50 +11,57 @@ type Params = {
 };
 
 const upResume: ServiceMethodFn<Params> = async ({ page }) => {
-  const methodInfo = await getMethodInfo({
-    methodName,
-    serviceName: info.name,
-  });
+  try {
+    const methodInfo = await getMethodInfo({
+      methodName,
+      serviceName: info.name,
+    });
 
-  if (!methodInfo) {
-    throw new Error("Failed to get method info");
-  }
-
-  const { method, service, baseUrl } = methodInfo;
-
-  await page.goto(baseUrl, { timeout: gotoTimeout });
-  await sleep(1000);
-  logger.debug("[up-resume]: Page Opened");
-
-  await page.waitForSelector('div[data-qa="resume"]');
-  const resumes = await page.$$('div[data-qa="resume"]');
-  logger.debug("[up-resume]: Resumes collected");
-
-  for (const index in resumes) {
-    const upButton = await resumes[index].$(
-      'button[data-qa="resume-update-button_actions"]'
-    );
-
-    if (!upButton) {
-      logger.warn("[up-resume]: Resume update button was not found");
-      break;
+    if (!methodInfo) {
+      throw new Error("Failed to get method info");
     }
 
-    await upButton.click();
-    await sleep(1500);
+    const { method, service, baseUrl } = methodInfo;
+
+    await page.goto(baseUrl, { timeout: gotoTimeout });
+    await sleep(1000);
+    logger.debug("[up-resume]: Page Opened");
+
+    await page.waitForSelector('div[data-qa="resume"]');
+    const resumes = await page.$$('div[data-qa="resume"]');
+    logger.debug("[up-resume]: Resumes collected");
+
+    for (const index in resumes) {
+      const upButton = await resumes[index].$(
+        'xpath///span[contains(text(), "Поднять в поиске")]'
+      );
+
+      if (!upButton) {
+        logger.warn("[up-resume]: Resume update button was not found");
+        break;
+      }
+
+      logger.debug(upButton);
+      upButton.click();
+      await sleep(1500);
+
+      logger.debug("[up-resume]: Resume update button clicked");
+    }
+
+    await page.close();
+    logger.debug("[up-resume]: Page Closed");
+
+    const inserted = await db
+      .insert(schema.serviceData)
+      .values({ serviceId: service.id, methodId: method.id, data: [] })
+      .returning();
+
+    const insertedData = inserted[0];
+
+    return { data: [], insertedId: insertedData.id };
+  } catch (e) {
+    console.log(e);
   }
-
-  await page.close();
-  logger.debug("[up-resume]: Page Closed");
-
-  const inserted = await db
-    .insert(schema.serviceData)
-    .values({ serviceId: service.id, methodId: method.id, data: [] })
-    .returning();
-
-  const insertedData = inserted[0];
-
-  return { data: [], insertedId: insertedData.id };
 };
 
 export { upResume, methodName };
